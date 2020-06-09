95e63054b22d333f1e058554ef1b52b4228a1a96
Deeprom S <grooveeeeee@gmail.com>
2020-06-09 17:22:36 +0700

    add TestCalculatorSum

--- cmd/simple-service/main_test.go
+++ cmd/simple-service/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -22,6 +23,27 @@ func TestGetRequest(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 
 	expected := `{"alive": true}`
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+	assert.Equal(t, expected, rr.Body.String())
+}
+
+func TestCalculatorSum(t *testing.T) {
+	t.Parallel()
+
+	var jsonReq = []byte(`{"a": 100, "b": 200.2}`)
+	req, err := http.NewRequest("POST", "/calculator.sum", bytes.NewBuffer(jsonReq))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(Alive)
+
+	handler.ServeHTTP(rr, req)
+
+	expected := `{"result": 300.2}`
 
 	assert.Equal(t, http.StatusOK, rr.Code)
 	assert.Equal(t, expected, rr.Body.String())
